8c0ffc9728a27167dff5c6158233156028288cd3
MitchellBarure <m.barure@alustudent.com>
2026-01-22 16:27:43 +0200

    Addition of DML Statements for the Users table

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -1,4 +1,4 @@
-#Create table to store basic user information
+#Use DDL Statements to create table to store basic user information
 CREATE TABLE Users (
     User_id INT AUTO_INCREMENT PRIMARY KEY,
     Full_name VARCHAR(100) NOT NULL,
@@ -7,3 +7,9 @@ CREATE TABLE Users (
     User_Type ENUM ('Customer', 'Agent', 'Merchant'),
     Keywords TEXT NOT NULL
 );
+
+#Example of some DML Statements to insert data into the table
+INSERT  INTO Users(User_id, Full_name, Phone_Number, Account_Number, User_Type, Keywords)
+VALUES
+    (1, 'MitchellBarure', 0794467731, 9568899732,'Customer', '1 MitchellBarure, 0794467731, 9568899732, Customer'),
+    (2, 'JohnMichael', 0893349976, 235179334, 'Agent', '2, JohnMichael, 0893347796, Agent');
