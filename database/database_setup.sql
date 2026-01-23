a2805437827707d5ce1a41b709c42c60968b2406
Mich-O <m.odhiambo@alustudent.com>
2026-01-23 10:46:49 +0200

    system_logs table setup

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -1,15 +1,16 @@
-#Use DDL Statements to create table to store basic user information
-CREATE TABLE Users (
-    User_id INT AUTO_INCREMENT PRIMARY KEY,
-    Full_name VARCHAR(100) NOT NULL,
-    Phone_Number VARCHAR(15) UNIQUE,
-    Account_Number VARCHAR(20) NULL, #NULL ensures that the table still operates even if a user does not have an Account_number
-    User_Type ENUM ('Customer', 'Agent', 'Merchant'), 
-    Keywords TEXT NOT NULL #Search Indexing to ensure user can find data faster
+#DDL Statements to create system logs table
+CREATE TABLE system_logs (
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    raw_message TEXT NOT NULL, #Stores the full XML string 
+    processing_status ENUM('pending', 'success', 'failed') DEFAULT 'pending', #Track the lifecycle of the data
+    error TEXT DEFAULT NULL, #Field to store error details if status is 'failed'
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    processed_at TIMESTAMP NULL DEFAULT NULL
 );
 
-#Example of some DML Statements to insert data into the table
-INSERT  INTO Users(User_id, Full_name, Phone_Number, Account_Number, User_Type, Keywords)
+#Sample DML statements to populate the system_logs table
+INSERT INTO system_logs (raw_message, processing_status)
 VALUES
-    (1, 'MitchellBarure', 0794467731, 9568899732,'Customer', '1 MitchellBarure, 0794467731, 9568899732, Customer'),
-    (2, 'JohnMichael', 0893349976, 235179334, 'Agent', '2, JohnMichael, 0893347796, Agent');
+    ('<sms protocol="0" address="M-Money" date="1715351458724" type="1" subject="null" body="You have received 2000 RWF from Jane Smith (*********013) on your mobile money account at 2024-05-10 16:30:51. Message from sender: . Your new balance:2000 RWF. Financial Transaction Id: 76662021700." toa="null" sc_toa="null" service_center="+250788110381" read="1" status="-1" locked="0" date_sent="1715351451000" sub_id="6" readable_date="10 May 2024 4:30:58 PM" contact_name="(Unknown)" />',
+    'pending')
+    ('transaction message sample 2''failed');
