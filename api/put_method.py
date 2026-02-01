1be262e6160231d16d4e4770a79a16b94aafdaa0
L-nsamba <l.nsamba@alustudent.com>
2026-02-01 17:13:19 +0200

    transactions json test with put

--- api/put_method.py
+++ api/put_method.py
@@ -53,24 +53,18 @@ class putTransaction(BaseHTTPRequestHandler):
         with open(DATA_FILE, "r") as data_file:
             transactions = json.load(data_file)
 
-        transaction_dict = {}
+        updated = False
+        # Updating only transactions in the JSON file which have an id
         for transaction in transactions:
-            if "id" in transaction:
-                transaction_dict[str(transaction["id"])] = transaction
-
-        if transaction_id not in transaction_dict:
-            self.send_response(401)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps({"error": "Transaction not Found"}).encode())
-            return
-
-        transaction_dict[transaction_id].update(updated_fields)
+            if str(transaction.get("id")) == transaction_id:
+                transaction.update(updated_fields)
+                updated = True
+                break
 
         # Saving the updated list back to the file
 
         with open(DATA_FILE, "w") as data_file:
-            json.dump(list(transaction_dict.values()), data_file, indent=4)
+            json.dump(transactions, data_file, indent=4)
 
         #Success responses
         self.send_response(200)
@@ -78,7 +72,6 @@ class putTransaction(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(json.dumps({
             "message": "Transaction successfully updated",
-            "transaction": transaction_dict[transaction_id]
         }).encode())
 
 if __name__ == "__main__":
